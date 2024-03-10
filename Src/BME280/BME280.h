621a2c7e50ee44c9074dd1b982303e0d0dc3321a
NickTheBit <nikosgklu@gmail.com>
2024-03-10 09:10:59 +0100

    Updated FreeRTOS

--- Src/BME280/BME280.h
+++ Src/BME280/BME280.h
@@ -1,47 +1,50 @@
 /**********************************************
-* @brief  Bare-bone driver for the BME280_DEFS sensor.
-* @author Nikolaos Xenofon Gkloumpos
-* @date   18/09/2023
-**********************************************/
+ * @brief  Bare-bone driver for the BME280_DEFS sensor.
+ * @author Nikolaos Xenofon Gkloumpos
+ * @date   18/09/2023
+ **********************************************/
 
 #pragma once
 
-#include "Interfaces/I2CInterface.hh"
 #include "BME280_defs.hh"
+#include "Interfaces/I2CInterface.hh"
 
 struct filteredMeasurements {
-   double pressure;
-   double temperature;
-   double humidity;
+	double pressure;
+	double temperature;
+	double humidity;
 };
 
 struct rawMeasurements {
-   uint32_t pressure;
-   uint32_t temperature;
-   uint32_t humidity;
+	uint32_t pressure;
+	uint32_t temperature;
+	uint32_t humidity;
 };
 
 class BME280 {
- private:
-   I2CInterface &i2cBus;
-   uint8_t address;
-   uint16_t pollingRate;
-   struct filteredMeasurements currentMeasurements = {};
+  private:
+	I2CInterface &i2cBus;
+	uint8_t address;
+	uint16_t pollingRate;
+	struct filteredMeasurements currentMeasurements = {};
 
-   [[noreturn]] static void pollingTask(void * arguments);
-   void readRegister(uint8_t registerAddress, uint8_t *returnedData, uint32_t length);
-   void writeRegister(uint8_t registerAddress, uint8_t *dataToWrite, uint32_t length);
- public:
-   BME280(I2CInterface &i2cBus, uint8_t address, uint16_t pollingRate);
-   ~BME280() = default;
+	[[noreturn]] static void pollingTask(void *arguments);
+	void readRegister(uint8_t registerAddress, uint8_t *returnedData,
+					  uint32_t length);
+	void writeRegister(uint8_t registerAddress, uint8_t *dataToWrite,
+					   uint32_t length);
 
-   BME280_DEFS::sensorPowerMode_t getSensorMode();
-   void setSensorMode(BME280_DEFS::sensorPowerMode_t newState);
-   void softReset();
-   void getSensorData();
-   uint8_t getChipID();
+  public:
+	BME280(I2CInterface &i2cBus, uint8_t address, uint16_t pollingRate);
+	~BME280() = default;
 
-   double getTemperature();
-   double getHumidity();
-   double getPressure();
+	BME280_DEFS::sensorPowerMode_t getSensorMode();
+	void setSensorMode(BME280_DEFS::sensorPowerMode_t newState);
+	void softReset();
+	void getSensorData();
+	uint8_t getChipID();
+
+	double getTemperature();
+	double getHumidity();
+	double getPressure();
 };
--- Src/PrimaryTask/PrimaryTask.cpp
+++ Src/PrimaryTask/PrimaryTask.cpp
@@ -12,26 +12,25 @@
 #include "BME280/BME280.h"
 
 void primaryTask(void *arguments) {
-    // Configure the Pico's on-board LED as a heartbeat indicator.
+	// Configure the Pico's on-board LED as a heartbeat indicator.
 
-    auto heartbeatPin = RP2040_GPIOPin(PICO_DEFAULT_LED_PIN);
-    auto i2cBus1 = RP2040_I2C(i2c0, 20, 21, false, 1000 * 100);
+	auto heartbeatPin = RP2040_GPIOPin(PICO_DEFAULT_LED_PIN);
+	auto i2cBus1 = RP2040_I2C(i2c0, 20, 21, false, 1000 * 100);
 
 	auto bme = BME280(i2cBus1, 0x76, 10);
 
+	while (true) {
 
+		printf("ChipID: %02X\n\r", bme.getChipID());
+		double humid = bme.getHumidity();
+		double temperature = bme.getTemperature();
+		double pressure = bme.getPressure();
+		printf("Humidity: %2.2f\tTemperature: %2.2f\tPressure: %2.2f\n\r", humid, temperature, pressure);
 
-    while (true) {
-
-		// double humid = bme.getHumidity();
-		// double temperature = bme.getTemperature();
-		// double pressure = bme.getPressure();
-		// printf("Humidity: %2.2f\tTemperature: %2.2f\tPressure: %2.2f\n\r", humid, temperature, pressure);
-
-        // Starting a heartbeat to indicate proper initialization.
-        heartbeatPin.write(HAL::GPIO_STATE::LOW);
-        vTaskDelay(1000);
-        heartbeatPin.write(HAL::GPIO_STATE::HIGH);
-        vTaskDelay(50);
-    }
+		// Starting a heartbeat to indicate proper initialization.
+		heartbeatPin.write(HAL::GPIO_STATE::LOW);
+		vTaskDelay(1000);
+		heartbeatPin.write(HAL::GPIO_STATE::HIGH);
+		vTaskDelay(50);
+	}
 }
