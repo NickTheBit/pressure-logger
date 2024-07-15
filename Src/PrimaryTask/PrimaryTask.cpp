3a823b30c2c8c99fa9da3931827178608332f0ee
nick <nikosgklu@gmail.com>
2024-07-15 20:31:46 +0200

    Work in progress UART and logging implementation.

--- Src/PrimaryTask/PrimaryTask.cpp
+++ Src/PrimaryTask/PrimaryTask.cpp
@@ -6,13 +6,18 @@
  *********************************************/
 
 #include "PrimaryTask.hh"
-#include "main/main.h"
 #include "Architectures/RP2040/PR2040_GPIO/RP2040_GPIOPin.hh"
 #include "Architectures/RP2040/RP2040_I2C/RP2040_I2C.h"
+#include "Architectures/RP2040/RP2040_UART/RP2040_UART.hh"
+#include "hardware/uart.h"
 #include "BME280/BME280.h"
+#include "Logging/Logging.hh"
+#include "main/main.h"
 
 [[noreturn]] void primaryTask(void *arguments) {
 	// Configure the Pico's on-board LED as a heartbeat indicator.
+	auto uartBus = RP2040_UART(*uart0, 115200);
+	auto logger = Logging(uartBus);
 
 	auto heartbeatPin = RP2040_GPIOPin(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 	auto i2cBus1 = RP2040_I2C(i2c0, 20, 21, false, 1000 * 100);
@@ -21,11 +26,13 @@
 
 	while (true) {
 
-		printf("ChipID: %02X\n\r", bme.getChipID());
-		double humid = bme.getHumidity();
-		double temperature = bme.getTemperature();
-		double pressure = bme.getPressure();
-		printf("Humidity: %2.2f\tTemperature: %2.2f\tPressure: %2.2f\n\r", humid, temperature, pressure);
+		auto strOut = "ChipID: %02X\n\r" + std::to_string(bme.getChipID());
+		logger.logWrite(Logging::INFO, strOut);
+
+//		double humid = bme.getHumidity();
+//		double temperature = bme.getTemperature();
+//		double pressure = bme.getPressure();
+//		printf("Humidity: %2.2f\tTemperature: %2.2f\tPressure: %2.2f\n\r", humid, temperature, pressure);
 
 		// Starting a heartbeat to indicate proper initialization.
 		heartbeatPin.write(HAL::GPIO_STATE::LOW);
