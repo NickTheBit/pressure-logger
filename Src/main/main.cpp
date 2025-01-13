d21a319a77c4e4c134e2a7643e7ca4a2fca2725d
nick <nick@createitreal.com>
2025-01-13 23:05:00 +0100

    Successfully implemented static memory in tasks.

--- Src/main/main.cpp
+++ Src/main/main.cpp
@@ -9,9 +9,6 @@
 #include "main.h"
 #include "PrimaryTask/PrimaryTask.hh"
 
-// Creating task handles.
-TaskHandle_t primaryTaskHandle = nullptr;
-
 /*
  * RUNTIME START
  */
@@ -20,21 +17,20 @@ int main() {
 	stdio_init_all();
 
 	// Setting up primary Task.
-	BaseType_t pico_status = xTaskCreate(primaryTask, "primaryTask", 128,
-										 nullptr, 1, &primaryTaskHandle);
+	static StackType_t primaryTask_puxBuffer[128];
+	static StaticTask_t primaryTask_tcbBuffer;
+
+	xTaskCreateStatic(primaryTask, "primaryTask", 128, nullptr, 1, primaryTask_puxBuffer, &primaryTask_tcbBuffer);
 
 	// Reporting Firmware info on UART
 	printf("*************************************\n\t%s\n----------------------"
 		   "---------------\nVersion: %d.%d.%d\nCommit hash: "
 		   "%x\n*************************************\n",
-		   PROJECT_NAME, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR,
-		   PROJECT_VERSION_PATCH, GIT_HASH);
+		   PROJECT_NAME, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH, GIT_HASH);
 
 	// Start the FreeRTOS scheduler
 	// Only proceed with valid tasks
-	if (pico_status == pdPASS) {
-		vTaskStartScheduler();
-	}
+	vTaskStartScheduler();
 
 	// If the scheduler doesn't take over, it should be handled here.
 	while (true) {
